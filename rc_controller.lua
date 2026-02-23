a4bde14ac152e277aeb33d9895ced3e223512548
VoxelPartum <81391820+VoxelPartum@users.noreply.github.com>
2026-02-23 15:35:14 -0500

    Update rc_controller.lua
    screw the http request, im using websockets now, which *feels* easier, but im still struggling.

--- rc_controller.lua
+++ rc_controller.lua
@@ -1,13 +1,25 @@
-curX,curY,curZ = gps.locate()
---print("Current Coordinates:", curX, curY, curZ)
 
-coords = {}
-for i=0,3 do
---coords[i] = read()
+
+local ws, err = http.websocket("ws://localhost:8080")
+
+if err then
+	print(err)
+elseif ws then
+	while true do
+		term.clear()
+		term.setCursorPos(1,1)
+		print("Running VosX v0.1")
+		--wsRequest.send(string.format("Turtle's coords are: %d, %d, %d.", curX, curY, curZ))
+		local rawMessage = ws.recieve()
+		if rawMessage == nil then
+			break
+		end
+		local message = textUtils.unserialiseJSON(rawMessage)
+		if message == nil then
+			print("womp")
+			break
+		else
+			print(message)
+		end
+	end
 end
-
---print((curX-desX),(curY-desY),(curZ-desZ))
-
-local request = http.get("http://127.0.0.1:5000/")
-print(request.readAll())
-request.close()

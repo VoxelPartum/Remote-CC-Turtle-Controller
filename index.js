0dfc48466a6467462271a1d5a6a0a73b0bc89f77
VoxelPartum <81391820+VoxelPartum@users.noreply.github.com>
2026-02-28 01:40:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+const socket = require("ws");
 var clients = [];
+const webSocket = new socket.Server({port:5656});
+
+webSocket.on("connection", wsClient => {
+	console.log("Something connected");
+	
+	clients.push(wsClient);
+	
+	wsClient.on("message", messageData => {
+		console.log("recieved message: "+messageData);
+	})
+	
+	wsClient.on("close",()=>{
+		console.log("Something disconnected");
+		clients.pop(wsClient);
+		
+	})
+})var clients = [];
 const webSocket = new WebSocket("ws://127.0.0.1:8080");
 
 

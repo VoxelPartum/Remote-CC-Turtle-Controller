864e355f7bfab72b8e2e095e52fbb132aa7738d6
VoxelPartum <81391820+VoxelPartum@users.noreply.github.com>
2026-04-01 01:14:10 -0400

    Better Server

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const socket = require("ws");
+
+const webSocket = new socket.Server({port:5656});
+
+console.log("Opening server now.")
+var connectionCount = 0;
+var clients = [];
+
+webSocket.on("connection", wsClient => {
+	
+	
+	connectionCount++;
+	clients.push(wsClient);
+	
+	console.log("Something connected, number of connections: "+connectionCount);
+	
+	wsClient.on("message", messageData => {
+		console.log("recieved message: "+messageData.toString());
+		
+		clients.forEach(function(client){
+            var jsonData = JSON.stringify(messageData.toString(), null, 4)
+            client.send(messageData.toString())
+        })
+		
+	})
+	
+	wsClient.on("close",()=>{
+		console.log("Something disconnected");
+		connectionCount--;
+	})
+})const socket = require("ws");
 var clients = [];
 const webSocket = new socket.Server({port:5656});
 
